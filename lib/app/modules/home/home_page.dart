3843871fd97c2a81751a47f6d87cf7e8794314a6
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2020-04-27 22:47:25 -0300

    menus de abas

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -8,9 +8,38 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
   Color whatsAppGreen = Color.fromRGBO(18, 140, 126, 1.0);
   Color whatsAppGreenLight = Color.fromRGBO(37, 211, 102, 1.0);
+
+  TabController tabController;
+  var fabIcon = Icons.message;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    tabController = TabController(vsync: this, length: 4)
+      ..addListener(() {
+        setState(() {
+          switch (tabController.index) {
+            case 0:
+              break;
+            case 1:
+              fabIcon = Icons.message;
+              break;
+            case 2:
+              fabIcon = Icons.camera_enhance;
+              break;
+            case 3:
+              fabIcon = Icons.call;
+              break;
+          }
+        });
+      });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,6 +61,26 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
         backgroundColor: whatsAppGreen,
+        bottom: TabBar(
+          tabs: [
+            Tab(
+              icon: Icon(Icons.camera_alt),
+            ),
+            Tab(
+              child: Text("CHATS"),
+            ),
+            Tab(
+                child: Text(
+              "STATUS",
+            )),
+            Tab(
+                child: Text(
+              "CALLS",
+            )),
+          ],
+          indicatorColor: Colors.white,
+          controller: tabController,
+        ),
       ),
       body: Column(
         children: <Widget>[],
