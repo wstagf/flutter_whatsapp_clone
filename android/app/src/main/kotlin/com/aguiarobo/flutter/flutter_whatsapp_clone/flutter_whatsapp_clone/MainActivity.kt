330f3fecca94db27ec8c91535a7c68f46113e55e
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2020-04-27 22:26:59 -0300

    Criado projeto

---
+++ android/app/src/main/kotlin/com/aguiarobo/flutter/flutter_whatsapp_clone/flutter_whatsapp_clone/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.aguiarobo.flutter.flutter_whatsapp_clone.flutter_whatsapp_clone
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
